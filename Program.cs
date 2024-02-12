f2f7affa8081e0105dd27cd16195bb35de1fad63
Miryoqub <salimovmiryoqub@gmail.com>
2024-02-12 20:51:06 +0500

    Salom

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+Console.Write("Son kiriting :");
+
+int inputNum = Convert.ToInt32(Console.ReadLine());
+
+int chec = 4  ;
+
+for(int i = inputNum;chec>0;i++ )
+{
+    int tupChecker=0;
+    for(int j=1;j<=i;j++)
+    {
+       if(i%j==0)
+       {
+         tupChecker++;
+       }
+       
+       
+    }
+    if(tupChecker==2)
+       {
+        Console.WriteLine(i);
+        chec--;
+       }
+ 
+}
+
